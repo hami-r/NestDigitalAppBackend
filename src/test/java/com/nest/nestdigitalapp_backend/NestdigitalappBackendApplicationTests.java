5af51ed83687efec1861222ffc11083ac1c59617
hami-r <mynameissaitama54@gmail.com>
2022-12-12 12:02:05 +0530

    initial commit

---
+++ src/test/java/com/nest/nestdigitalapp_backend/NestdigitalappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
