a41360de064e23ef49d4983f0f4c20c493ac41d0
hami-r <mynameissaitama54@gmail.com>
2022-12-12 12:20:52 +0530

    controller package

---
+++ src/main/java/com/nest/nestdigitalapp_backend/controller/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.nest.nestdigitalapp_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+}
---
+++ src/main/java/com/nest/nestdigitalapp_backend/controller/LogController.java
@@ -0,0 +1,4 @@
+package com.nest.nestdigitalapp_backend.controller;
+
+public class LogController {
+}
